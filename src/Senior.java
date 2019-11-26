dad421e64c26fbf476e82a1c53b9f29fa72602a0
osamahan999 <35288172+osamahan999@users.noreply.github.com>
2019-11-26 14:22:27 -0800

    Senior page built. All visitor inheritance tree done

---
+++ src/Senior.java
@@ -0,0 +1,28 @@
+
+public class Senior extends Visitor {
+
+	private int ticketAmt;
+	
+	public Senior(String firstName, String middleName, String lastName, int ticketAmt, int age) {
+		super(firstName, middleName, lastName, age);
+
+		this.ticketAmt = ticketAmt;
+	}
+
+	public Senior(String firstName, String middleName, String lastName, int ticketAmt, int age, String streetAddress, String city,
+			String state, String country, int zipcode, String monthBorn, int dayBorn, int yearBorn) {
+		super(firstName, middleName, lastName, age, streetAddress, city, state, country, zipcode, monthBorn, dayBorn,
+				yearBorn);
+
+		this.ticketAmt = ticketAmt;
+	}
+
+	public int getTicketAmt() {
+		return ticketAmt;
+	}
+
+	public void setTicketAmt(int ticketAmt) {
+		this.ticketAmt = ticketAmt;
+	}
+
+}
