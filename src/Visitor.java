3c8579bfab8cb29312492032c765750cbebe1e6d
osamahan999 <35288172+osamahan999@users.noreply.github.com>
2019-11-26 13:46:58 -0800

    Visitor class

---
+++ src/Visitor.java
@@ -0,0 +1,29 @@
+
+public class Visitor extends Human {
+
+	private int age;
+
+
+	public Visitor(String firstName, String middleName, String lastName, int age) {
+		super(firstName, middleName, lastName);
+		this.age = age;
+	}
+
+	public Visitor(String firstName, String middleName, String lastName, int age, String streetAddress, String city,
+			String state, String country, int zipcode, String monthBorn, int dayBorn, int yearBorn) {
+		
+		super(firstName, middleName, lastName, streetAddress, city, state, country, zipcode, monthBorn, dayBorn, yearBorn);
+		this.age = age;	
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+
+}
