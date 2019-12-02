62a650606a3865121a52b76c01d1d753ed270cce
Nithin Kumar <super0blue0@gmail.com>
2019-12-02 04:35:54 -0800

    Fixed Employee Login and Details

--- src/EmployeeDetails.java
+++ src/EmployeeDetails.java
@@ -29,6 +29,28 @@ public class EmployeeDetails extends Application {
 		text1.setStyle("-fx-font-size: 25;");
 		Text text3 = new Text("Employee Type: " + displayEmp.getWorkType() + "\nPay: " + displayEmp.getSalary());
 		text1.setStyle("-fx-font-size: 25;");
+		
+		Button backButton = new Button();
+		backButton.setText("Back");
+		backButton.setMaxHeight(200);
+		backButton.setStyle("-fx-background-color: \n" + "        #090a0c,\n"
+				+ "        linear-gradient(#38424b 0%, #1f2429 20%, #191d22 100%),\n"
+				+ "        linear-gradient(#20262b, #191d22),\n"
+				+ "        radial-gradient(center 50% 0%, radius 100%, rgba(114,131,148,0.9), rgba(255,255,255,0));\n"
+				+ "    -fx-background-radius: 5,4,3,5;\n" + "    -fx-background-insets: 0,1,2,0;\n"
+				+ "    -fx-text-fill: white;\n"
+				+ "    -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 );\n"
+				+ "    -fx-font-family: \"Arial\";\n" + "    -fx-text-fill: linear-gradient(white, #d0d0d0);\n"
+				+ "    -fx-font-size: 12px;\n" + "    -fx-padding: 10 20 10 20;");
+		backButton.setOnAction(e -> {
+			SignInEmployee login = new SignInEmployee();
+			try {
+				login.start(primaryStage);
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		});
 
 		GridPane gridPane = new GridPane();
 		gridPane.setAlignment(Pos.TOP_CENTER);
@@ -38,6 +60,7 @@ public class EmployeeDetails extends Application {
 		gridPane.add(text1, 1, 0);
 		gridPane.add(text2, 1, 1);
 		gridPane.add(text3, 2, 1);
+		gridPane.add(backButton, 2, 2);
 
 		
 		gridPane.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;" + "-fx-border-width: 2;"
