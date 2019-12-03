4c3ec62ac14fddf14d77c49ddb1f16d4c0bf9a04
osamahan999 <hanhan.osama@gmail.com>
2019-12-02 17:23:15 -0800

    Hire employee button removed

--- src/OwnerPage.java
+++ src/OwnerPage.java
@@ -86,23 +86,7 @@ public class OwnerPage extends Application {
 		});
 
 
-		// Hire employees
-		Button hireEmployees = new Button();
-		hireEmployees.setText("Hire new employees");
-		hireEmployees.setStyle("-fx-background-color: \n" +
-				"        #090a0c,\n" +
-				"        linear-gradient(#38424b 0%, #1f2429 20%, #191d22 100%),\n" +
-				"        linear-gradient(#20262b, #191d22),\n" +
-				"        radial-gradient(center 50% 0%, radius 100%, rgba(114,131,148,0.9), rgba(255,255,255,0));\n" +
-				"    -fx-background-radius: 5,4,3,5;\n" +
-				"    -fx-background-insets: 0,1,2,0;\n" +
-				"    -fx-text-fill: white;\n" +
-				"    -fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 );\n" +
-				"    -fx-font-family: \"Arial\";\n" +
-				"    -fx-text-fill: linear-gradient(white, #d0d0d0);\n" +
-				"    -fx-font-size: 12px;\n" +
-				"    -fx-padding: 10 20 10 20;");
-		hireEmployees.setMaxHeight(20);
+	
 
 		// Fire Employees
 		Button fireEmployees = new Button();
@@ -156,7 +140,6 @@ public class OwnerPage extends Application {
 		gridPane.add(collectPay, 0, 1);
 		gridPane.add(payFromVisitors, 0, 2);
 		gridPane.add(givePay, 0, 3);
-		gridPane.add(hireEmployees, 0, 4);
 		gridPane.add(alert, 1, 3);
 		gridPane.add(button, 2, 1);
 
